efdcbe11a19bed958ffd020d5fd7b8ff853baa8c
jmyorston <77361832+jmyorston@users.noreply.github.com>
2023-06-11 20:31:53 +0100

    ex1.3

---
+++ Chapter1/excercises/ex1-3.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (square x) (* x x)) 
+  
+(define (squareSum x y) (+ (square x) (square y))) 
+ 
+(define (sumOfLargestTwoSquared x y z) 
+  (cond ((and (>= (+ x y) (+ y z)) (>= (+ x y) (+ x z))) (squareSum x y)) 
+        ((and (>= (+ x z) (+ y z)) (>= (+ x z) (+ x y))) (squareSum x z)) 
+        (else (squareSum y z)) 
+  ) 
+) 
+
+(sumOfLargestTwoSquared 3 4 1)
+
