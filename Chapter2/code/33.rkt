80bca081e29b8d71e541ed840fade76cba581ab5
James <jmyorston@hotmail.com>
2023-08-23 21:37:48 +0100

    2.37

--- Chapter2/code/33.rkt
+++ Chapter2/code/33.rkt
@@ -68,3 +68,52 @@
                (enumerate-tree t))))
 
 (count-leaves (list 1 2 (list 3 4 5) 4 5 (list 1 (list 3))))
+
+
+; -------------------------------------
+;           Exercise 2.36
+; -------------------------------------
+(displayln "*** Exercise 2.36 ***")
+
+(define s '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      null
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 s)
+
+; '(22 26 30)
+
+; -------------------------------------
+;           Exercise 2.37
+; -------------------------------------
+(displayln "*** Exercise 2.37 ***")
+
+
+(define (dot-product v w)
+  (accumulate + 0 (map * 'v w)))
+
+(define (matrix-*-vector m v)
+  (map (λ (r)(dot-product v r)) m))
+
+(define (transpose mat)
+  (accumulate-n cons null mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (λ (x) (map (λ (y) (dot-product x y)) cols)) m)))
+
+(matrix-*-vector s1 v1)
+
+
+
+
+
+
+
+
+
+
